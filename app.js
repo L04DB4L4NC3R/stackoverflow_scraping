98fcde37a37ac2bd36b51fa33f915e638bce579f
angad <angadsharma1016@gmail.com>
2018-07-13 10:10:53 +0530

    scraper working

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-const scraper = require("./scrape");
-
-let obj = scraper(1,"nodemon","text");
-
-
-obj.scrape("a[class=question-hyperlink]")
-.then(console.log)
-.catch(console.log);
+const functions = require("./functions")
