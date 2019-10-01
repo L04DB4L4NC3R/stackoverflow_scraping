04f9695529944f41cfbcb6e00170cafc46fa2070
VVishvam <v2vishvam@gmail.com>
2019-10-01 11:51:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,14 +5,6 @@ const {
 
 const top = require("./top");
 
-// questions("nodemon")
-// .then(console.log)
-// .catch(console.log);
-
-// latest("http error 404")
-// .then(console.log)
-// .catch(console.log);
-
 top()
 .then(console.log)
 .catch(console.log);
